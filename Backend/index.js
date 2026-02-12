db058cb15827868965fb6e363fd0ebec39813441
Aman Ruhela <amank004433@gmail.com>
2026-02-12 23:50:15 +0530

    shifting to railway

--- Backend/index.js
+++ Backend/index.js
@@ -48,7 +48,7 @@ app.get("/ping", (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
